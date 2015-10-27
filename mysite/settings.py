99ca9703edcee708b4ecb62de2e49cb19a86de94
cineron <cineron+gh@gmail.com>
2015-10-27 01:15:38 -0500

    changed settings file to use sqlite3 database and updated .gitignore

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,22 +75,22 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "djangogirls",
-        "USER": "ron",
-        # "PASSWORD": "",
-        # "HOST": "localhost",
-        # "PORT": "",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": "djangogirls",
+#         "USER": "ron",
+#         # "PASSWORD": "",
+#         # "HOST": "localhost",
+#         # "PORT": "",
+#     }
+# }
 
 
 # Internationalization
